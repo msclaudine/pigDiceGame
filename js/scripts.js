f860e994be19a83e6c805158902a183e45fd29ab
musaclo@gmail.com <musaclo@gmail.com>
2019-01-11 10:09:19 +0200

    document reading and playes roll button from busines logic

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,40 @@ Player.prototype.restart = function() {
   this.turnScore = 0;
   this.dice.innerHTML = 0;
 };
+
+
+$(document).ready(function() {
+  var newPlayer1 = new Player ();
+  var newPlayer2 = new Player ();
+  var newPlayerName = new Player ();
+  $("form#names-row").submit(function(event) {
+    event.preventDefault();
+    newPlayerName.pName();
+    $("#p1-name").text(newPlayerName.name1);
+    $("#p2-name").text(newPlayerName.name2);
+    $("#intro-content").hide();
+    $("#content").show();
+  });
+  $("#p1-roll").click(function() {
+    newPlayer1.rollDice();
+    $("#p1-turnscore").text(newPlayer1.turnScore);
+  });
+  $("#p1-hold").click(function() {
+    newPlayer1.hold();
+    newPlayerName.pName();
+    $("#p1-score").text(newPlayer1.totalScore);
+    $("#p1-turnscore").text(0);
+    if (newPlayer1.totalScore >= 100) {
+      $("#winner-name").text(newPlayerName.name1 + " Wins!");
+      $("#win").fadeToggle();
+      $("#content").fadeToggle();
+    } else {
+      $("#turn-message").text("It's " + newPlayerName.name2 + "'s turn!");
+      $("#p1-roll, #p1-hold").prop("disabled", true);
+      $("#p2-roll, #p2-hold").prop("disabled", false);
+    }
+  });
+  $("#p2-roll").click(function() {
+    newPlayer2.rollDice();
+    $("#p2-turnscore").text(newPlayer2.turnScore);
+  });
